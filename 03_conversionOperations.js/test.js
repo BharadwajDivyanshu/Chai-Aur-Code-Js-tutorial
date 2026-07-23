5ede1d27f4d16014558c43616b38749a0a196628
Divyanshu Bharadwaj <154587653+BharadwajDivyanshu@users.noreply.github.com>
2026-07-23 22:44:08 +0530

    basic operation

---
+++ 03_conversionOperations.js/test.js
@@ -0,0 +1,24 @@
+
+let score = "333" ;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInScore = Number(score);
+console.log(typeof valueInScore);
+console.log(valueInScore);
+
+//"33" => 33;
+//"33ff" =>NaN
+//undefined => Nan
+//null => 0;
+//true=>1 false=> 0
+
+
+let isStudent="uf";
+let booleanIsStudent = Boolean(isStudent);
+console.log(booleanIsStudent);
+
+// 1 => true  0=> false
+//"" => false //"dibu" => true
+//null or undefined => false;
